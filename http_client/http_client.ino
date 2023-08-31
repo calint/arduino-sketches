7aceaf0bf98d4e8dfe248cf1c4dce882872f7aa3
calint <calin.tenitchi@gmail.com>
2023-08-31 21:32:05 +0200

    .

--- http_client/http_client.ino
+++ http_client/http_client.ino
@@ -16,7 +16,7 @@ void setup() {
   while (!Serial)
     ;
   WiFi.mode(WIFI_STA);
-  WiFi.setHostname("RasberryPicoW");
+  //  WiFi.setHostname("RasberryPicoW");
   Serial.printf("connecting to '%s' with '%s'\n", STASSID, STAPSK);
   WiFi.begin(STASSID, STAPSK);
   while (WiFi.status() != WL_CONNECTED) {
@@ -31,12 +31,10 @@ void setup() {
   digitalWrite(LED_BUILTIN, HIGH);
 }
 
-void print_astronauts_in_space_right_now() {
-  digitalWrite(LED_BUILTIN, LOW);
-
+void get_json_from_web(const char* url, DynamicJsonDocument& json_doc) {
   HTTPClient http_client;
-  if (!http_client.begin(ASTROS_URL)) {
-    Serial.printf("unable to connect to %s\n", ASTROS_URL);
+  if (!http_client.begin(url)) {
+    Serial.printf("unable to connect to %s\n", url);
     return;
   }
   const auto http_code = http_client.GET();
@@ -46,7 +44,6 @@ void print_astronauts_in_space_right_now() {
     return;
   }
 
-  DynamicJsonDocument json_doc(8 * 1024);
   const auto json_error = deserializeJson(json_doc, http_client.getStream());
   if (json_error) {
     Serial.printf("json parsing failed: %s\n", json_error.c_str());
@@ -54,14 +51,17 @@ void print_astronauts_in_space_right_now() {
   }
 
   http_client.end();
+}
 
+void print_astronauts_in_space_right_now() {
+  digitalWrite(LED_BUILTIN, LOW);
+  DynamicJsonDocument json_doc(8 * 1024);
+  get_json_from_web(ASTROS_URL, json_doc);
   digitalWrite(LED_BUILTIN, HIGH);
-
   const auto people = json_doc["people"].as<JsonArray>();
   for (const auto p : people) {
     Serial.println(p["name"].as<const char*>());
   }
-
   //const unsigned n = json_doc["number"].as<unsigned>();
   //for (unsigned i = 0; i < n; i++) {
   //  Serial.println(json_people[i]["name"].as<const char*>());
@@ -70,32 +70,10 @@ void print_astronauts_in_space_right_now() {
 
 void print_current_time_based_on_ip() {
   digitalWrite(LED_BUILTIN, LOW);
-
-  HTTPClient http_client;
-  if (!http_client.begin(TIME_SERVER_URL)) {
-    Serial.printf("unable to connect to %s\n", TIME_SERVER_URL);
-    return;
-  }
-  const auto http_code = http_client.GET();
-  if (http_code != HTTP_CODE_OK) {
-    Serial.printf("GET failed, error: %s\n", http_client.errorToString(http_code).c_str());
-    http_client.end();
-    return;
-  }
-
   DynamicJsonDocument json_doc(1024);
-  const auto json_error = deserializeJson(json_doc, http_client.getStream());
-  if (json_error) {
-    Serial.printf("json parsing failed: %s\n", json_error.c_str());
-    return;
-  }
-
-  http_client.end();
-
+  get_json_from_web(TIME_SERVER_URL, json_doc);
   digitalWrite(LED_BUILTIN, HIGH);
-
   const auto date_time = json_doc["datetime"].as<String>();
-
   //  "2023-08-31T16:32:47.653086+02:00" to "2023-08-31 16:32:47"
   const auto date_time_fmt = date_time.substring(0, 10) + " " + date_time.substring(11, 19);
   Serial.println(date_time_fmt);

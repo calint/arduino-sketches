e9f64da8e233eec85e8fa5064a2524a2e7c94a78
calint <calin.tenitchi@gmail.com>
2023-09-20 18:53:49 +0200

    .

--- pwm/pwm.ino
+++ pwm/pwm.ino
@@ -7,27 +7,27 @@ const int ledPin = LED_GREEN;
 const int freq = 5000;
 const int ledChannel = 0;
 const int resolution = 8;
- 
-void setup(){
+
+void setup() {
   // configure LED PWM functionalitites
   ledcSetup(ledChannel, freq, resolution);
-  
+
   // attach the channel to the GPIO to be controlled
   ledcAttachPin(ledPin, ledChannel);
 }
- 
-void loop(){
+
+void loop() {
   // increase the LED brightness
-  for(int dutyCycle = 0; dutyCycle <= 255; dutyCycle++){   
+  for (int dutyCycle = 0; dutyCycle <= 255; dutyCycle++) {
     // changing the LED brightness with PWM
     ledcWrite(ledChannel, dutyCycle);
     delay(15);
   }
 
   // decrease the LED brightness
-  for(int dutyCycle = 255; dutyCycle >= 0; dutyCycle--){
+  for (int dutyCycle = 255; dutyCycle >= 0; dutyCycle--) {
     // changing the LED brightness with PWM
-    ledcWrite(ledChannel, dutyCycle);   
+    ledcWrite(ledChannel, dutyCycle);
     delay(15);
   }
 }

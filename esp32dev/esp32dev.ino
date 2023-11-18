97eeddbf6ede46f00f1f36911b4700cce66f9656
calint <calin.tenitchi@gmail.com>
2023-11-18 12:00:28 +0100

    .

--- esp32dev/esp32dev.ino
+++ esp32dev/esp32dev.ino
@@ -111,15 +111,13 @@ public:
 
 } static fps{};
 
-// palette used to convert uint8_t to uint16_t rgb 565 color
+// palette used to convert uint8_t to uint16_t rgb 565
+// lower and higher byte swapped (red being the highest bits)
 static constexpr uint16_t palette[256]{
     0b0000000000000000, // black
-    0b0000000000011111, // green
-    0b0000011111100000, // red
-    0b0000011111111111, // yellow
-    0b1111100000000000, // blue
-    0b1111100000011111, // cyan
-    0b1111111111100000, // magenta
+    0b0000000011111000, // red
+    0b1110000000000111, // green
+    0b0001111100000000, // blue
     0b1111111111111111, // white
 };
 

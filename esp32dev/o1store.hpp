50df9f48fe92fd216ce488976a9e93b3945a747c
calint <calin.tenitchi@gmail.com>
2023-11-26 14:20:08 +0100

    .

--- esp32dev/o1store.hpp
+++ esp32dev/o1store.hpp
@@ -6,6 +6,9 @@
 // * Size is number of pre-allocated objects
 // * IxType is type used to index in lists and should be unsigned with a bit
 //   width that fits 'Size'
+// * StoreId is for debugging
+// * InstanceSizeInBytes is custom size of instance
+//   used to fit largest object in an object hierarchy in lack of std::variant
 //
 // example:
 //   Type = sprite, Size = 255, IxType = uint8_t gives 255 allocatable sprites
@@ -18,12 +21,15 @@ template <typename Type, const unsigned Size, typename IxType,
           const unsigned StoreId = 0, const unsigned InstanceSizeInBytes = 0>
 class o1store {
   Type *all_ = nullptr;
-  IxType *free_ = nullptr;
-  IxType free_ix_ = 0;
-  IxType *alloc_ = nullptr;
+  IxType *free_bgn_ = nullptr;
+  IxType *free_ptr_ = nullptr;
+  IxType *free_end_ = nullptr;
+  IxType *alloc_bgn_ = nullptr;
+  IxType *alloc_ptr_ = nullptr;
   IxType alloc_ix_ = 0;
-  IxType *del_ = nullptr;
-  IxType del_ix_ = 0;
+  IxType *del_bgn_ = nullptr;
+  IxType *del_ptr_ = nullptr;
+  IxType *del_end_ = nullptr;
 
 public:
   o1store() {
@@ -32,16 +38,19 @@ public:
     } else {
       all_ = (Type *)calloc(Size, sizeof(Type));
     }
-    free_ = (IxType *)calloc(Size, sizeof(IxType));
-    alloc_ = (IxType *)calloc(Size, sizeof(IxType));
-    del_ = (IxType *)calloc(Size, sizeof(IxType));
-    if (!all_ or !free_ or !alloc_ or !del_) {
+    free_ptr_ = free_bgn_ = (IxType *)calloc(Size, sizeof(IxType));
+    free_end_ = free_bgn_ + Size;
+    alloc_ptr_ = alloc_bgn_ = (IxType *)calloc(Size, sizeof(IxType));
+    del_ptr_ = del_bgn_ = (IxType *)calloc(Size, sizeof(IxType));
+    del_end_ = del_bgn_ + Size;
+    if (!all_ or !free_bgn_ or !alloc_bgn_ or !del_bgn_) {
       Serial.printf("!!! o1store %u: could not allocate arrays\n", StoreId);
       while (true)
         ;
     }
-    for (unsigned i = 0; i < Size; i++) {
-      free_[i] = i;
+    IxType i = 0;
+    for (IxType *it = free_bgn_; it < free_end_; i++, it++) {
+      *it = i;
     }
   }
 
@@ -53,50 +62,53 @@ public:
   // }
 
   // returns true if allocatable instances available
-  auto can_allocate() -> bool { return free_ix_ < Size; }
+  inline auto can_allocate() -> bool { return free_ptr_ < free_end_; }
 
   // allocates an instance
   auto allocate_instance() -> Type * {
-    if (free_ix_ == Size) {
+    if (free_ptr_ >= free_end_) {
       return nullptr;
     }
-    IxType ix = free_[free_ix_];
-    alloc_[alloc_ix_] = ix;
+    IxType ix = *free_ptr_;
+    free_ptr_++;
+    *alloc_ptr_ = ix;
+    alloc_ptr_++;
     Type *inst = instance(ix);
     inst->alloc_ix = alloc_ix_;
-    free_ix_++;
     alloc_ix_++;
     return inst;
   }
 
   // adds instance to a list that is applied with 'apply_free()'
   void free_instance(const Type *inst) {
-    if (del_ix_ == Size) {
-      Serial.printf("!!! o1store %u: free overrun\n", StoreId);
+    if (del_ptr_ >= del_end_) {
+      Serial.printf("!!! o1store %u: free overrun [alloc_ix=%u]\n", StoreId,
+                    inst->alloc_ix);
       while (true)
         ;
     }
-    del_[del_ix_++] = alloc_[inst->alloc_ix];
+    *del_ptr_ = alloc_bgn_[inst->alloc_ix];
+    del_ptr_++;
   }
 
   // de-allocates the instances that have been freed
   void apply_free() {
-    IxType *it = del_;
-    for (unsigned i = 0; i < del_ix_; i++, it++) {
+    for (IxType *it = del_bgn_; it < del_ptr_; it++) {
       Type *inst_deleted = instance(*it);
-      IxType inst_ix_to_move = alloc_[alloc_ix_ - 1];
+      alloc_ptr_--;
+      IxType inst_ix_to_move = *alloc_ptr_;
       Type *inst_to_move = instance(inst_ix_to_move);
       inst_to_move->alloc_ix = inst_deleted->alloc_ix;
-      alloc_[inst_deleted->alloc_ix] = inst_ix_to_move;
-      alloc_ix_--;
-      free_ix_--;
-      free_[free_ix_] = *it;
+      alloc_bgn_[inst_deleted->alloc_ix] = inst_ix_to_move;
+      free_ptr_--;
+      *free_ptr_ = *it;
     }
-    del_ix_ = 0;
+    alloc_ix_ -= (del_ptr_ - del_bgn_);
+    del_ptr_ = del_bgn_;
   }
 
   // returns pointer to list of allocated instances
-  inline auto allocated_list() -> IxType * { return alloc_; }
+  inline auto allocated_list() -> IxType * { return alloc_bgn_; }
 
   // returns size of list of allocated instances
   inline auto allocated_list_len() -> IxType { return alloc_ix_; }

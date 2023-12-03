b6bf141c5bdec97bbc042eebe6242dd0dab78a84
calint <calin.tenitchi@gmail.com>
2023-12-03 11:12:25 +0100

    .

--- esp32dev/utils/png-to-resources/extract-to-resources.sh
+++ esp32dev/utils/png-to-resources/extract-to-resources.sh
@@ -2,8 +2,8 @@
 set -e
 cd $(dirname "$0")
 
-./read-palette sprites.png > ../../game/resources/palette_sprites.hpp
-./read-sprites sprites.png > ../../game/resources/sprite_imgs.hpp
+./read-palette.py sprites.png > ../../game/resources/palette_sprites.hpp
+./read-sprites.py sprites.png > ../../game/resources/sprite_imgs.hpp
 
-./read-palette tiles.png > ../../game/resources/palette_tiles.hpp
-./read-sprites tiles.png > ../../game/resources/tile_imgs.hpp
+./read-palette.py tiles.png > ../../game/resources/palette_tiles.hpp
+./read-sprites.py tiles.png > ../../game/resources/tile_imgs.hpp
--- esp32dev/utils/png-to-resources/make.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-
-clang++ -o read-palette read-palette.cpp -lpng
-clang++ -o read-sprites read-sprites.cpp -lpng
